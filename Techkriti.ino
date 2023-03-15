ae18257827709a9d2cc5b2db531b6496d08c3af4
Sam Varghese <sam.varg.2003@gmail.com>
2023-03-15 21:49:23 +0530

    Update Techkriti.ino

--- Techkriti.ino
+++ Techkriti.ino
@@ -1,59 +1,85 @@
-#include<Servo.h>
+#include <Servo.h> // Include the servo library
 
-Servo reactionInitiator;  
+Servo chemicalReleaser;  // create servo object to control a servo
+Servo lander;  // create servo object to control a servo
 
-// Define pins
-const int triggerPin = 7;
-const int echoPin = 6;
-const int ledPin = 13;
+const int chemicalReleaserPin = 8;
+const int landerPin = 10;
+const bool oneTimeRunner = true;
+const int trigPin = 7;
+const int echoPin = 4;
+// Distance (in cm) to kept before initiating the reaction and activating the landers
+const int actionDistance = 20;
 
-// Define variables
-long duration;
-int distance;
+// For resetting the state of the components
+void resetComponents(){
+  chemicalReleaser.write(0);
+  lander.write(0);
+}
+
+// For testing the components
+void tester(){
+  resetComponents();
+
+  delay(1000);
+
+  Serial.println("Moving the chemical releaser...");
+  chemicalReleaser.write(90);
+  Serial.println("Moving the landers...");
+  lander.write(90);
+
+  delay(1000);
+
+  resetComponents();
+}
+
+// Send a pulse to the ultrasonic sensor to measure the distance with object in front of it
+int measureDistance(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Read the time it takes for the pulse to return
+  long duration = pulseIn(echoPin, HIGH);
+
+  // Calculate the distance in centimeters
+  int distance = duration / 58;
+
+  return distance;
+}
 
 void setup() {
-  // Initialize serial communication
   Serial.begin(9600);
-
-  reactionInitiator.attach(9, 500, 2500);
-  reactionInitiator.write(90);
-
-  // Set pin modes
-  pinMode(triggerPin, OUTPUT);
+  pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(ledPin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT); // For controlling thw builtin LED
+  chemicalReleaser.attach(chemicalReleaserPin);  // attaches the servo on pin 10 to the servo object
+  lander.attach(landerPin);  // attaches the servo on pin 10 to the servo object
+
+  resetComponents();
 }
 
 void loop() {
-  // Generate ultrasonic pulse
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
+  int objectDist = measureDistance();
 
-  // Measure duration of echo pulse
-  duration = pulseIn(echoPin, HIGH);
-
-  // Calculate distance in cm
-  distance = duration * 0.034 / 2;
-
-  // Print distance to serial monitor
-  Serial.print("Distance: ");
-  Serial.print(distance);
+  Serial.print("Distance with the object: ");
+  Serial.print(objectDist);
   Serial.println(" cm");
+  
+  if(objectDist < actionDistance){
+    chemicalReleaser.write(90);
+    lander.write(90);
 
-  // Turn on LED if distance is less than 100cm
-  if (distance < 100) {
-    digitalWrite(ledPin, HIGH);
-    reactionInitiator.write(90);
-    delay(5000);
-    reactionInitiator.write(0);
+    delay(3000);
+
+    resetComponents();
+
   } else {
-    digitalWrite(ledPin, LOW);
-    reactionInitiator.write(0);
+    resetComponents();
   }
 
-  // Wait for 100ms before next measurement
-  delay(100);
+  // Wait before taking the next reading
+  delay(500);
 }
